45fc22043d8fbb86040d9808a4096376547b313d
Matehun111 <151171402+Matehun111@users.noreply.github.com>
2026-03-22 17:17:08 +0100

    Add files via upload

--- server.js
+++ server.js
@@ -83,7 +83,8 @@ function sign_ticket(key, hwid, plan, ticket_exp, nonce) {
 }
 
 app.use(cors())
-app.use(express.json({ limit: "10mb" }))
+app.use(express.json({ limit: "50mb" }))
+app.use(express.urlencoded({ limit: "50mb", extended: true }))
 
 // ── POST /admin/create ────────────────────────────────────────────────────
 app.post('/admin/create', async (req, res) => {
